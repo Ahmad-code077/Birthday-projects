908cca505fd63a4115f2efca6482d30152e7bda2
Ahmad Mamoon <146346428+Ahmad-code077@users.noreply.github.com>
2024-05-31 01:10:29 +0500

    Update App.js
    I have change some code that after the list is cleared the user should reload the file I added a reload button

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+import './index.css';
 function App() {
   const [people, setPeople] = useState(data);
+
   return (
-    <main>
-      <section className='container'>
-        <h2>{people.length} Birthday Today </h2>
-        <List people={people} />
-        <button
-          onClick={() => {
-            setPeople([]);
-          }}
-        >
-          Clear all{' '}
-        </button>
-      </section>
-    </main>
+    <>
+      <main>
+        <div className='container'>
+          <h2>{people.length} birthday today</h2>
+          <List people={people} setPeople={setPeople} />
+          <button
+            className='btn'
+            onClick={
+              people.length === 0
+                ? () => {
+                    window.location.reload(true);
+                  }
+                : () => setPeople([])
+            }
+          >
+            {people.length === 0 ? 'Reload' : 'Clear All'}
+          </button>
+        </div>
+      </main>
+    </>
   );
 }
 
 export default App;
+
